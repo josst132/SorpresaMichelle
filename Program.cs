cf8fab88491190ef1485a9e2ec954c2ee862683e
josst132 <patrick.goyez@uisek.edu.ec>
2025-08-19 23:37:06 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.MapFallbackToFile("index.html");
 
-app.MapGet("/", async context =>
-{
-    context.Response.Redirect("/index.html");
-});
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://*:{port}");
 
 app.Run();
